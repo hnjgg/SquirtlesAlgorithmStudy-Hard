499f5d924e1e077db6afa1789e30f3ac4acb413a
Minseo Kang <81473751+Mingosnake@users.noreply.github.com>
2024-10-08 21:20:44 +0900

    [민서] 프로그래머스 연속펄스부분수열의합

---
+++ 민서/연속펄스부분수열의합.py
@@ -0,0 +1,10 @@
+def solution(sequence):
+    end = len(sequence)
+    if end == 1:
+        return abs(sequence[0])
+    for i in range(1, end, 2):
+        sequence[i] = -sequence[i]
+    cumsum = [0]
+    for num in sequence:
+        cumsum.append(cumsum[-1] + num)
+    return abs(max(cumsum) - min(cumsum))
